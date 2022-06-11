303b37662b0526f44492713a0bb446abddc68809
a <a>
2022-06-11 12:59:15 -0300

    initial commit

---
+++ src/main/java/br/com/vendaj/venda/VendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.vendaj.venda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vendaj/venda/VendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vendaj.venda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
