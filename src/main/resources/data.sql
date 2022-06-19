a54cd8a1663b2a4131f99a38c20aae6f5dda3f02
a <a>
2022-06-19 13:34:02 -0300

    table population

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,19 @@
-INSERT INTO categoria(nome, img, descricao) VALUES ('AFF', 'img1', 'AAAAAAAAAAAAAA');
-INSERT INTO categoria(nome, img, descricao) VALUES ('BFF', 'img2', 'BBBBBBBBBBBBB');
-INSERT INTO categoria(nome, img, descricao) VALUES ('CFF', 'img3', 'CCCCCCCCCCCCC');
-INSERT INTO categoria(nome, img, descricao) VALUES ('DFF', 'img4', 'DDDDDDDDDDDDD');
-INSERT INTO categoria(nome, img, descricao) VALUES ('EFF', 'img5', 'EEEEEEEEEEEEE');
+INSERT INTO categoria(nome, img, descricao) VALUES ('Bebidas', 'IMG/bebidas.png', 'Liquidos');
+INSERT INTO categoria(nome, img, descricao) VALUES ('Comidas', 'IMG/comidas.png', 'comestiveis');
+INSERT INTO categoria(nome, img, descricao) VALUES ('Doces', 'IMG/doces.png', 'comestiveis açucarados');
 
-INSERT INTO produto (img, nome, descricao, categoria, valor) VALUES ('img1', 'Teste1', 'AAAAAAAAAAAAA', 'AFF', 420.69);
-INSERT INTO produto (img, nome, descricao, categoria, valor) VALUES ('img2', 'Teste2', 'BBBBBBBBBBBBB', 'AFF', 69.69);
-INSERT INTO produto (img, nome, descricao, categoria, valor) VALUES ('img3', 'Teste3', 'CCCCCCCCCCCCC', 'BFF', 420.42);
-INSERT INTO produto (img, nome, descricao, categoria, valor) VALUES ('img4', 'Teste4', 'DDDDDDDDDDDDD', 'CFF', 42.69);
-INSERT INTO produto (img, nome, descricao, categoria, valor) VALUES ('img5', 'Teste5', 'EEEEEEEEEEEEE', 'DFF', 69.42);
-INSERT INTO produto (img, nome, descricao, categoria, valor) VALUES ('img6', 'Teste6', 'FFFFFFFFFFFFF', 'EFF', 690.42);
+INSERT INTO produto (img, nome, descricao, categoria_nome, valor) VALUES ('IMG/pepsi.png', 'Pepsi', 'refrigerante de cola', 'Bebidas', 7.10);
+INSERT INTO produto (img, nome, descricao, categoria_nome, valor) VALUES ('IMG/guarana.png', 'Guarana', 'refrigerante de guarana', 'Bebidas', 6.50);
+INSERT INTO produto (img, nome, descricao, categoria_nome, valor) VALUES ('IMG/fanta-laranja.png', 'Fanta Laranja', 'refrigerante de laranja', 'Bebidas', 7.50);
+INSERT INTO produto (img, nome, descricao, categoria_nome, valor) VALUES ('IMG/soda.png', 'Soda', 'refrigerante de limão', 'Bebidas', 5.90);
+
+INSERT INTO produto (img, nome, descricao, categoria_nome, valor) VALUES ('IMG/feijoada.png', 'Feijoada', 'feijão com carne de porco', 'Comidas', 29.90);
+INSERT INTO produto (img, nome, descricao, categoria_nome, valor) VALUES ('IMG/frango-assado.png', 'Frango', 'frango assado', 'Comidas', 28.50);
+INSERT INTO produto (img, nome, descricao, categoria_nome, valor) VALUES ('IMG/coxinha.png', 'Coxinha', 'salgado com frango', 'Comidas', 6.30);
+INSERT INTO produto (img, nome, descricao, categoria_nome, valor) VALUES ('IMG/sushi.png', 'Sushi', 'peixe cru com arroz', 'Comidas', 18.70);
+
+INSERT INTO produto (img, nome, descricao, categoria_nome, valor) VALUES ('IMG/sorvete.png', 'Sorvete', 'sorvete gelado', 'Doces', 10.50);
+INSERT INTO produto (img, nome, descricao, categoria_nome, valor) VALUES ('IMG/fini.png', 'Fini', 'fini de morango', 'Doces', 11.25);
+INSERT INTO produto (img, nome, descricao, categoria_nome, valor) VALUES ('IMG/bolo.png', 'Bolo', 'bolo de chcolate', 'Doces', 25.50);
+INSERT INTO produto (img, nome, descricao, categoria_nome, valor) VALUES ('IMG/bolacha.png', 'Bolacha', 'bolacha de chocolate', 'Doces', 8.90);
+INSERT INTO produto (img, nome, descricao, categoria_nome, valor) VALUES ('IMG/brigadeiro.png', 'Brigadeiro', 'brigadeiro de chocolate', 'Doces', 4.40);
